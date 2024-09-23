3829b7942d2a822958f159abd970dac638832d7f
Khalil Hamdi <khalil@nterone.com>
2024-09-23 15:56:15 +0100

    Adding and using a grid system

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 use bevy::prelude::*;
 
 const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
 #[derive(Component)]
 struct SnakeHead;
+
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -21,25 +44,53 @@ fn spawn_snake(mut commands: Commands) {
             },
             ..default()
         })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
+}
+
+fn size_scaling(windows: Query<&Window>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_single().unwrap();
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Query<&Window>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_single().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
 }
 
 fn snake_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+    mut head_positions: Query<&mut Position, With<SnakeHead>>,
 ) {
-    for mut transform in head_positions.iter_mut() {
+    for mut position in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= 2.;
+            position.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::ArrowRight) {
-            transform.translation.x += 2.;
+            position.x += 1;
         }
         if keyboard_input.pressed(KeyCode::ArrowDown) {
-            transform.translation.y -= 2.;
+            position.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::ArrowUp) {
-            transform.translation.y += 2.;
+            position.y += 1;
         }
     }
 }
@@ -48,7 +99,8 @@ fn main() {
     App::new()
         .add_systems(Startup, setup_camera) // <--
         .add_systems(Startup, spawn_snake)
-        .add_systems(Update, snake_movement) // <--
+        .add_systems(Update, snake_movement)
+        .add_systems(PostUpdate, (position_translation, size_scaling).chain()) // TO VERIFY LATER //<--
         .add_plugins(DefaultPlugins)
         .run();
 }
